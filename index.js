0809186e661bee9e6b6a16e9a0aed0c9767c46ac
Javier OrBe <javier.ortiz@monou.gg>
2023-10-07 09:14:48 -0600

    actualizacion 071023

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World2');
+  res.send('Hello World new demo');
 })
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-})
+module.exports = app;
